543b8907dd333e8a46b09f4e1eb00655cb7108f1
Lucas Bataglião <lucasbatagliao@Lucass-MacBook-Pro.local>
2017-12-21 21:24:07 -0200

    changed to arrow functions

--- server/api/controllers/storeController.js
+++ server/api/controllers/storeController.js
@@ -1,11 +1,11 @@
 // This file defines the actions of server for routes defined in ../routes/storeRoutes.js
 
 // route: GET /stores 
-exports.list_all_stores = function(req, res){
+exports.list_all_stores = (req, res) => {
     res.json({message: 'all stores'});
 };
 
 // route: POST /stores
-exports.add_store = function(req, res){
+exports.add_store = (req, res) => {
     res.json({message: 'not implemented'});
 };
