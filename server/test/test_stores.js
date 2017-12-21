3f8c818706de802cb76bca068bded4e6a860f06b
Lucas Bataglião <lucasbatagliao@Lucass-MacBook-Pro.local>
2017-12-21 20:28:52 -0200

    tests for store - without delete and update

--- server/test/test_stores.js
+++ server/test/test_stores.js
@@ -1,3 +1,5 @@
+import { request } from 'http';
+
 //During the test the env variable is set to test
 process.env.NODE_ENV = 'test';
 
@@ -18,7 +20,9 @@ db.connect(`${__dirname}/data`);
 db.loadCollections(['stores']);
 
 describe('Stores', () => {
-    beforeEach((done) => { // clear stores database before each test
+
+    // runs before all tests in this block
+    before('clearing stores db', (done) => { // clear stores database before all tests
         return db.stores.remove({}).then(done());
     });
 
@@ -49,6 +53,120 @@ describe('Stores', () => {
                     done();
                 });
         });
+
+        it('should not add a Store with empty code', (done) => {
+            var store = new Store();
+            store.description = 'some store name';
+            store.postalcode = 140100000;
+
+            chai.request(server)
+                .post('/api/stores/')
+                .set('x-admin', true)
+                .send(store)
+                .end((err, res) => {
+                    expect(res).to.have.status(400);
+                    done();
+                })
+        })
+
+        it('should not add a Store without name', (done) => {
+            var store = new Store();
+            store.code = 1
+            store.postalcode = 140100000;
+
+            chai.request(server)
+                .post('/api/stores/')
+                .set('x-admin', true)
+                .send(store)
+                .end((err, res) => {
+                    expect(res).to.have.status(400);
+                    done();
+                })
+        })
+
+        it('should not add a Store without postal code', (done) => {
+            var store = new Store();
+            store.code = 1
+            store.description = 'some store name';
+
+            chai.request(server)
+                .post('/api/stores/')
+                .set('x-admin', true)
+                .send(store)
+                .end((err, res) => {
+                    expect(res).to.have.status(400);
+                    done();
+                })
+        })
+
+        it('should accept a valid new store', (done) => {
+            var store = new Store();
+            store.code = 1
+            store.description = 'store n1';
+            store.postalcode = 140100000;
+
+            chai.request(server)
+                .post('/api/stores/')
+                .set('x-admin', true)
+                .send(store)
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    done();
+                })
+        })
+
+        it('should reject a store with same code', (done) => {
+            var store = new Store();
+            store.code = 1
+            store.description = 'store n1';
+            store.postalcode = 140100000;
+
+            chai.request(server)
+                .post('/api/stores/')
+                .set('x-admin', true)
+                .send(store)
+                .end((err, res) => {
+                    expect(res).to.have.status(500);
+                    expect(res).to.have.message.equal(`duplicated store ${store.code}`);
+                    done();
+                })
+        })
+        
+    });
+
+    describe('/GET stores', () => {
+
+        before('clearing stores db', (done) => { // clear stores database before all GET tests
+            return db.stores.remove({}).then(done());
+        });
+
+        it('should get an empty array with a clean DB', (done) => {
+            chai.request(server)
+                .get('/api/stores')
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('array');
+                    expect(res.body).to.have.lengthOf(0);
+                    done();
+                });
+        });
+
+        it('shoud return items after insert', (done) => {
+            var store = new Store();
+            store.code = 1;
+            store.description = 'store n1';
+            store.postalcode = 14010000;
+            repo.add(store);
+
+            chai.request(server)
+                .get('/api/stores')
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('array');
+                    expect(res.body).to.have.lengthOf(1);
+                });
+        });
+
     });
 
 });
