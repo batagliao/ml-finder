eed0bfbcb5c0280e1fcb450c5aac90ab7295f513
Lucas Bataglião <lucasbatagliao@Lucass-MacBook-Pro.local>
2017-12-24 18:48:03 -0200

    fix product detail componente

--- client/src/app/product-detail/product-detail.component.ts
+++ client/src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) { }
 
   ngOnInit() {
+    const prodId = this.route.snapshot.paramMap.get('id');
   }
 
 }
