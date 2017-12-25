c0bb1c383afb37e409afd9952fb62a9a691fbfb5
Lucas Bataglião <lucasbatagliao@Lucass-MacBook-Pro.local>
2017-12-25 20:23:20 -0200

    modal component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { ModalComponent } from './modal/modal.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('childModal') childModal: ModalComponent;
+
   title = 'app';
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing/app-routing.module';
-
 import { HttpClient } from '@angular/common/http';
+
+import { ModalDirective, ModalModule } from 'ngx-bootstrap';
 
 
 import { AppComponent } from './app.component';
@@ -16,18 +15,22 @@ import { ProductService } from './services/product.service';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { GeoLocationService } from './services/geolocation.service';
 
+import { ModalComponent } from './modal/modal.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PageNotFoundComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ModalModule.forRoot()
   ],
   providers: [
     ProductService,
