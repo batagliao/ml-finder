40ffc672aceb1d438ea1e76a67dfcb1da5e98dd0
Lucas Bataglião <lucasbatagliao@Lucass-MacBook-Pro.local>
2017-12-26 20:11:39 -0200

    geolocation tests for stores

--- server/test/test_storesController.js
+++ server/test/test_storesController.js
@@ -171,4 +171,136 @@ describe('Stores Controller', () => {
 
     });
 
+    describe('/GET closest stores by code', () => {
+
+        before('clearing stores and inserting', (done) => { // clear stores database before all GET tests
+            db.stores.remove();
+            db.loadCollections(['stores']);
+
+            db.stores.add({
+                "code": 1,
+                "description": "Loja Matriz",
+                "postalcode": 14400020,
+                "address": "Jesus Maria José, Franca - SP, 14400-020, Brazil",
+                "lat": -20.5401572,
+                "lng": -47.40518050000001
+            });
+            db.stores.add({
+                "code": 2,
+                "description": "Loja Sertãozinho",
+                "postalcode": 14160170,
+                "address": "R. Dr. Olidair Ambrósio - Centro, Sertãozinho - SP, 14160-170, Brazil",
+                "lat": -21.129585,
+                "lng": -47.988292
+            });
+            db.stores.add({
+                "code": 3,
+                "description": "Loja Cuiaba",
+                "postalcode": 78005030,
+                "address": "Araes, Cuiabá - MT, 78005-030, Brazil",
+                "lat": -15.5902348,
+                "lng": -56.09565389999999
+            });
+            db.stores.add({
+                "code": 4,
+                "description": "Loja Ribeirão",
+                "postalcode": 14010120,
+                "address": "R. Barão do Amazonas - Centro, Ribeirão Preto - SP, 14010-120, Brazil",
+                "lat": -21.1838026,
+                "lng": -47.81370219999999
+            });
+            db.stores.add({
+                "code": 5,
+                "description": "Loja São Paulo",
+                "postalcode": 1505010,
+                "address": "Sé, São Paulo - SP, 01505-010, Brazil",
+                "lat": -23.5539935,
+                "lng": -46.63110930000001
+            });
+            db.stores.add({
+                "code": 6,
+                "description": "Loja Curitiba",
+                "postalcode": 80010100,
+                "address":"Av. Visc. de Guarapuava - Centro, Curitiba - PR, 80010-100, Brazil",
+                "lat": -25.436828,
+                "lng": -49.2685786
+            });            
+            done();
+        });
+       
+        it('should get stores ordered by distance', (done) => {
+            chai.request(Server)
+                .get('/api/stores?code=1&code=2&code=3&code=4&code=5&code=6&lat=-21.12760065601982&lng=-47.844003817142315')
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.an('array');
+                    expect(res.body).to.have.lengthOf(6);
+                    store = res.body[0];
+                    expect(res.body.code).to.be.equal(4);
+                    store = res.body[1];
+                    expect(res.body.code).to.be.equal(2);
+                    store = res.body[2];
+                    expect(res.body.code).to.be.equal(1);
+                    store = res.body[3];
+                    expect(res.body.code).to.be.equal(5);
+                    store = res.body[4];
+                    expect(res.body.code).to.be.equal(6);
+                    store = res.body[5];
+                    expect(res.body.code).to.be.equal(3);
+                    done();
+                });
+        });
+
+        it('should work even if its passed only 1 store', (done) => {
+            chai.request(Server)
+                .get('/api/stores?code=1&clat=-21.12760065601982&lng=-47.844003817142315')
+                .end( (err, res) => {
+                    expe
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.an('array');
+                    expect(res.body).to.have.lengthOf(1);
+                    store = res.body[0];
+                    expect(res.body.code).to.be.equal(1);
+                });
+        });
+
+        it('should return all stores when no stores is provided', (done) => {
+            console.log('this test is covered by /GET stores');
+            expect(true).to.be.equal(true);
+            done();
+        });
+
+        it('should return stores selected not sorted when no location is provided', (done) => {
+            chai.request(server)
+            .get('/api/stores/code=1&code=2')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.a('array');
+                expect(res.body).to.have.lengthOf(2);
+                done();
+            });
+        });
+
+        it('should return stores selected not sorted when only latitude is provided', (done) => {
+            chai.request(server)
+            .get('/api/stores/code=1&code=2lat=-20.00')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.a('array');
+                expect(res.body).to.have.lengthOf(2);
+                done();
+            });
+        });
+
+        it('should return stores selected not sorted when only longitude is provided', (done) => {
+            chai.request(server)
+            .get('/api/stores/code=1&code=lng=-47.00')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.a('array');
+                expect(res.body).to.have.lengthOf(2);
+                done();
+            });
+        });
+    });
 });
